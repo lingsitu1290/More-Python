64a8397ee1d4fe8fae3c09ff3b799eb40ca169d3
Ling Situ <lingsitu1290@gmail.com>
2017-01-03 17:01:58 -0800

    Changed to item to start items API

--- flask-api2/app.py
+++ flask-api2/app.py
@@ -4,12 +4,32 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+# class Student(Resource):
+#     def get(self, name):
+#         return {'student': name}
+#
+#
+# api.add_resource(Student, '/student/<string:name>')
+
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        # When can't find the item, thus 404(not found)
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        # 201 status code is "created", 202 is "accepted"
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
