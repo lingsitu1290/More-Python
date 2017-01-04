3dd3259eb4e3d4d2b736edbebd719dbd7d59ef6c
Ling Situ <lingsitu1290@gmail.com>
2017-01-03 17:19:10 -0800

    Created items list

--- flask-api2/app.py
+++ flask-api2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -22,13 +22,19 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         # 201 status code is "created", 202 is "accepted"
         return item, 201
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 
 if __name__ == '__main__':
