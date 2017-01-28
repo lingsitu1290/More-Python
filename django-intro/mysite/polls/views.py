cd105f69c5f6b8c2f1a9f9d1e14f441c637462f3
Ling Situ <lingsitu1290@gmail.com>
2017-01-28 01:58:40 -0800

    Added somes views and associated urls

--- django-intro/mysite/polls/views.py
+++ django-intro/mysite/polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+from .models import Question
+
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Awesome job! This is the index page of our polls application.')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
